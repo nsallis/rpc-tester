11c94f52d0192c4f359f0f0474a6b9f8cb71f9bd
nsallis <n.sallis@hotmail.com>
2018-11-25 15:29:07 -0500

    add ui and json parsing

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"flag"
+	"encoding/json"
 	"fmt"
+	"github.com/conformal/gotk3/gtk"
+	// "github.com/nsallis/rpc-tester/callback"
 	"net/rpc"
-  "encoding/json"
+	"strings"
 )
 
 type Response struct {
@@ -12,30 +14,94 @@ type Response struct {
 }
 
 type Request struct {
-	Payload     string
+	Payload string
 }
 
 func main() {
-	portUsage := "Which port the rpc server is running on"
-	commandUsage := "Which command to invoke"
-	payloadUsage := "Payload to send"
-	var port string
-	var command string
-	var payload string
-	flag.StringVar(&port, "port", "3333", portUsage)
-	flag.StringVar(&port, "p", "3333", portUsage)
+	gtk.Init(nil)
 
-	flag.StringVar(&command, "command", "Endpoints.Handshake", commandUsage)
-	flag.StringVar(&command, "c", "Endpoints.Handshake", commandUsage)
+	builder, err := gtk.BuilderNew()
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	flag.StringVar(&payload, "payload", "", payloadUsage)
-	flag.StringVar(&payload, "P", "", payloadUsage)
+	builder.AddFromFile("justlabel.ui")
+	obj, err := builder.GetObject("window1")
+	if err != nil {
+		fmt.Println(err)
+	}
+	var window *gtk.Window
+	if _, ok := obj.(*gtk.Window); !ok {
+		panic("Window not found in .ui file")
+	}
+	window = obj.(*gtk.Window)
+	window.Connect("destroy", gtk.MainQuit)
 
-	flag.Parse()
+	sendButton, err := builder.GetObject("SendButton")
+	sendButton.(*gtk.Button).Connect("clicked", func() {
+		params := GetRequestParams(builder)
+		SendRequest(params, builder)
+		// TODO send request after getting params from GetRequestParams
+	})
 
+	window.ShowAll()
+	gtk.Main()
+
+	// callback.Init(builder)
+
+}
+
+type RequestParams struct {
+	Address      string
+	Port         string
+	Command      string
+	Payload      string
+	FormatAsJson bool
+}
+
+// TODO error handling
+func GetRequestParams(builder *gtk.Builder) RequestParams {
+	// request address
+	requestAddressObj, _ := builder.GetObject("RequestAddress")
+	requestAddress, _ := requestAddressObj.(*gtk.Entry).GetText()
+
+	// request port
+	requestPortObj, _ := builder.GetObject("RequestPort")
+	requestPort, _ := requestPortObj.(*gtk.Entry).GetText()
+
+	// request command
+	requestCommandObj, _ := builder.GetObject("RequestCommand")
+	requestCommand, _ := requestCommandObj.(*gtk.Entry).GetText()
+
+	// request text (payload)
+	requestTextObj, _ := builder.GetObject("RequestText")
+	requestTextBuffer, _ := requestTextObj.(*gtk.TextView).GetBuffer()
+	start, end := requestTextBuffer.GetBounds()
+	requestText, _ := requestTextBuffer.GetText(start, end, false)
+
+	// format as json
+	formatAsJsonObj, _ := builder.GetObject("FormatAsJson")
+	formatAsJson := formatAsJsonObj.(*gtk.CheckButton).GetActive()
+
+	params := RequestParams{
+		Address:      requestAddress,
+		Port:         requestPort,
+		Command:      requestCommand,
+		Payload:      requestText,
+		FormatAsJson: formatAsJson,
+	}
+	return params
+}
+
+func SendRequest(params RequestParams, builder *gtk.Builder) {
+	payload := params.Payload
+	if params.FormatAsJson {
+		payload = strings.Replace(payload, "\"", "\\\"", -1)
+		payload = strings.Replace(payload, "\n", "\\\n", -1)
+	}
 	var (
-		addr     = "localhost:" + port
-		request  = &Request{Payload: payload}
+		addr     = params.Address + ":" + params.Port
+		request  = &Request{Payload: params.Payload}
 		response = new(Response)
 	)
 	// Establish the connection to the adddress of the
@@ -51,14 +117,30 @@ func main() {
 	// Perform a procedure call (core.HandlerName == Handler.Execute)
 	// with the Request as specified and a pointer to a response
 	// to have our response back.
-	err = client.Call(command, request, response)
+	err = client.Call(params.Command, request, response)
 	if err != nil {
 		fmt.Printf("Could not call server: %v", err)
 	}
-  jsonResponse, err := json.Marshal(response)
-  if err != nil {
-    fmt.Println("Could not convert the response to json")
-    fmt.Printf("Response: %v\n", response)
-  }
-	fmt.Println("Response: " + string(jsonResponse))
+	jsonResponse, err := json.Marshal(response.Payload)
+	if err != nil {
+		fmt.Println("Could not convert the response to json")
+		fmt.Printf("Response: %v\n", response)
+	}
+	jsonText := strings.Replace(string(jsonResponse), "\\\"", "\"", -1)
+	jsonText = strings.Replace(jsonText, ",\"", ",\n\"", -1)
+	jsonText = strings.Replace(jsonText, "{\"", "{\n\"", -1)
+	if err != nil {
+		fmt.Sprintln("error pretty printing: %e", err)
+	}
+	fmt.Println("jsonText")
+	fmt.Println(jsonText)
+
+	jsonText = strings.Replace(string(jsonText), ":{", ":\n\t{", -1)
+
+	responseTextObj, _ := builder.GetObject("ResponseText")
+	responseTextBox := responseTextObj.(*gtk.TextView)
+	responseTextBuffer, _ := responseTextBox.GetBuffer()
+	responseTextBuffer.SetText(string(jsonText))
+	fmt.Println("Response: " + string(jsonText))
+
 }
